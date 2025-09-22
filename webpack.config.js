fdd8f154339b342bf084b713a816fbbe9ed6d49e
mateo <mateojdenis@gmail.com>
2025-09-22 19:42:23 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -44,11 +44,11 @@ module.exports = (_, argv) => ({
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "remote_politica",
+      name: "remote_ciencia",
       filename: "remoteEntry.js",
       remotes: {},
       exposes: {
-        "./MiComponentePolitica": "./src/MiCompPolitica.js",
+        "./MiComponenteCiencia": "./src/MiCompCiencia.js",
       },
       shared: {
         ...deps,
